1fc9aa8577e3a1eb203b48c50ad74cd11e4ea5d8
tbara <tbarabasz@noblyapps.net>
2020-10-03 20:38:53 -0700

    Add project files.

---
+++ EncryptingWithOpenSSL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EncryptingWithOpenSSL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
